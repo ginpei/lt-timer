6d0db6f92d4ac2eae1ab37fea5c85600d3e461c5
ginpei <ginpei@gmail.com>
2016-08-26 21:38:19 -0700

    show rest time

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -1,7 +1,52 @@
 class IndexPage extends React.Component {
 	render() {
 		return (
-			<h1>Hello!!</h1>
+			<div>
+				<h1>Timelimit</h1>
+				<div>
+					<input type="hidden" value={this.state.period} />
+					<button onClick={this.startButton_onClick.bind(this)}>Start</button>
+					<button onClick={this.stopButton_onClick.bind(this)}>Stop</button>
+				</div>
+				<TimeRest rest={this.state.rest}></TimeRest>
+			</div>
 		);
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			period: 300000,  // 5m
+			rest: 0,
+		};
+	}
+
+	_startCountDown() {
+		this._stopCountDown();
+
+		this._tmCountDown = window.setInterval(()=>{
+			let now = Date.now();
+			this.setState({
+				rest: this.state.period - (now - this.state.startedAt),
+			});
+		}, 100);
+	}
+
+	_stopCountDown() {
+		window.clearInterval(this._tmCountDown);
+	}
+
+	startButton_onClick(event) {
+		this.setState({
+			startedAt: Date.now(),
+		});
+		this._startCountDown();
+	}
+
+	stopButton_onClick(event) {
+		this.setState({
+			startedAt: null,
+		});
+		this._stopCountDown();
 	}
 }
---
+++ src/TimeRest.js
@@ -0,0 +1,18 @@
+class TimeRest extends React.Component {
+	render() {
+		let rest = this.props.rest;
+		let restSec = Math.floor(rest / 1000);
+
+		let min = Math.floor(restSec / 60);
+		let sec = restSec % 60;
+		let sSec = `0${sec}`.slice(-2);  // 9 -> "09"
+
+		return (
+			<span>{min}:{sSec}</span>
+		);
+	}
+
+	constructor(props) {
+		super(props);
+	}
+}
