a86ffe93d0c42fa712b81ece1cae874905790923
ginpei <ginpei@gmail.com>
2017-03-27 00:53:11 -0700

    reboot

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import IndexPage from './IndexPage.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 document.addEventListener('DOMContentLoaded', (event)=>{
 	ReactDOM.render(
 		<IndexPage></IndexPage>,
