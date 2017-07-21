3186620eab174bb9f1274c9def1bb929b96c7d1e
ginpei <ginpei@gmail.com>
2017-07-21 00:13:49 -0700

    m:ss

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -16,7 +16,11 @@
 
 		computed: {
 			displayTime() {
-				return Math.ceil(this.time / 1000)
+				const all = Math.ceil(this.time / 1000)
+				const minutes = Math.floor(all / 60)
+				const seconds = all % 60
+				const filledSeconds = (seconds < 10 ? '0' : '') + seconds
+				return `${minutes}:${filledSeconds}`
 			},
 		},
 	}
