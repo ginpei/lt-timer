d5e393f3789eb7b8377ec409fa7ba86601639279
ginpei <ginpei@gmail.com>
2017-08-03 11:37:14 -0700

    router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const Vue = require('vue')
-const App = require('./components/App.vue')
+const App = require('./App.vue')
+const router = require('./router.js');
 
 new Vue({  // eslint-disable-line no-new
 	el: '#app',
+	router,
 	render: f=>f(App),
 })
